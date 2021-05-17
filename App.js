c0972ee003b18fadd0b4f50eb1e52dc644c43c8e
Arbab Hussain <zackh41@gmail.com>
2021-05-17 22:05:27 +0530

    complete user auth and setting screens

--- App.js
+++ App.js
@@ -1,19 +1,44 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { ThemeProvider } from "styled-components/native";
+import { theme } from "./src/infrastructure/theme";
+import { Navigation } from "./src/infrastructure/navigation";
+import firebase from "firebase/app";
 
+import "firebase/auth";
+//import "firebase/database";
+//import "firebase/firestore";
+//import "firebase/functions";
+// import "firebase/storage";
+
+// font import
 import {
   useFonts as useOswald,
   Oswald_400Regular,
 } from "@expo-google-fonts/oswald";
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
 
-import { theme } from "./src/infrastructure/theme";
-import { Navigation } from "./src/infrastructure/navigation";
-
+// contextProviders
 import { RestaurantsContextProvider } from "./src/services/restaurants/restaurants.context";
 import { LocationContextProvider } from "./src/services/location/location.context";
 import { FavouritesContextProvider } from "./src/services/favourites/favourites.context";
+import { AuthenticationContextProvider } from "./src/services/authentication/authentication.context";
+
+// Initialize Firebase
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyAtH_jmuMAKdMbns7RjoPe2SojFHRbM3JI",
+  authDomain: "mealstogo-ac9fd.firebaseapp.com",
+  projectId: "mealstogo-ac9fd",
+  storageBucket: "mealstogo-ac9fd.appspot.com",
+  messagingSenderId: "831710415845",
+  appId: "1:831710415845:web:7e2ba75b0b8c9161263d0c",
+};
+// Initialize Firebase
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
@@ -31,13 +56,9 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <FavouritesContextProvider>
-          <LocationContextProvider>
-            <RestaurantsContextProvider>
-              <Navigation />
-            </RestaurantsContextProvider>
-          </LocationContextProvider>
-        </FavouritesContextProvider>
+        <AuthenticationContextProvider>
+          <Navigation />
+        </AuthenticationContextProvider>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
